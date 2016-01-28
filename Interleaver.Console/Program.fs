464417c7d35a03118598f42a84f7a634cebb1d47
infosaurus <ianmcgog@gmail.com>
2016-01-28 21:51:09 +0100

    finish implementing synchronizeLoop

--- Interleaver.Console/Program.fs
+++ Interleaver.Console/Program.fs
@@ -2,14 +2,20 @@
 // See the 'F# Tutorial' project for more help.
 open Interleaver.Core.Interleave
 open Interleaver.Core.EventSourcing
+open System
 
 let mutable playerOneEvents: Event list = List.Empty
 let mutable playerTwoEvents: Event list = List.Empty
 let mutable playerThreeEvents: Event list = List.Empty
 
-let synchronizeTickMilliseconds = 2000
+let synchronizeTickMilliseconds = 10000
 
 let aggregateId = System.Guid.NewGuid()
+
+let colorizeNumber = function
+  | x when x % 2 = 0 -> Console.ForegroundColor <- ConsoleColor.DarkBlue 
+  | x when x % 3 = 0 -> Console.ForegroundColor <- ConsoleColor.DarkRed 
+  | _ -> Console.ForegroundColor <- ConsoleColor.DarkGreen
 
 let sendRandomCommand playerNumber =
 
@@ -34,6 +40,7 @@ let playerLoop playerNumber =
   let rnd = System.Random()
   while true do
     async  {
+      colorizeNumber playerNumber
       printfn "player number %d sending command..." playerNumber
       sendRandomCommand playerNumber
       do! Async.Sleep (rnd.Next(100, 1000))
@@ -43,60 +50,65 @@ let playerLoop playerNumber =
   }
 
 let synchronizeLoop =
-  let synchronizeAll
-    (aggregateId: AggregateId) 
-    (fitnessFunction: FitnessFunction) =
-    let player1Events = playerOneEvents
-    let player2Events = playerTwoEvents
-    let player3Events = playerThreeEvents
+  async {
+    let synchronizeAll
+      (aggregateId: AggregateId) 
+      (fitnessFunction: FitnessFunction) =
+      let player1Events = playerOneEvents
+      let player2Events = playerTwoEvents
+      let player3Events = playerThreeEvents
 
-    printfn "Beginning synchronization..."
-    printfn "Synchronizing player1 events..."
-    synchronize 
-      aggregateId 
-      (fun _ -> player1Events)
-      ([(fun _ -> player2Events); (fun _ -> player3Events)])
-      (fun events -> playerOneEvents <- events)
-      fitnessFunction
-    |> ignore
-    
-    printfn "Synchronizing player2 events..."
-    synchronize 
-      aggregateId 
-      (fun _ -> player2Events)
-      ([(fun _ -> player1Events); (fun _ -> player3Events)])
-      (fun events -> playerTwoEvents <- events)
-      fitnessFunction
-    |> ignore
-      
-    printfn "Synchronizing player3 events..."
-    synchronize 
-      aggregateId 
-      (fun _ -> player3Events)
-      ([(fun _ -> player2Events); (fun _ -> player1Events)])
-      (fun events -> playerThreeEvents <- events)
-      fitnessFunction
-    |> ignore
-
-    printfn "End of synchronization."
-
-  while true do
-    async {
-      do! Async.Sleep synchronizeTickMilliseconds
-      synchronizeAll 
+      Console.ForegroundColor <- ConsoleColor.Gray 
+      printfn "Beginning synchronization..."
+      printfn "Synchronizing player1 events..."
+      synchronize 
         aggregateId 
-        (function 
-        | SomePublicEvent event -> true
-        | _ -> false)
+        (fun _ -> player1Events)
+        ([(fun _ -> player2Events); (fun _ -> player3Events)])
+        (fun events -> playerOneEvents <- events)
+        fitnessFunction
       |> ignore
-    }
-    |> Async.RunSynchronously
-    |> ignore
+    
+      printfn "Synchronizing player2 events..."
+      synchronize 
+        aggregateId 
+        (fun _ -> player2Events)
+        ([(fun _ -> player1Events); (fun _ -> player3Events)])
+        (fun events -> playerTwoEvents <- events)
+        fitnessFunction
+      |> ignore
+      
+      printfn "Synchronizing player3 events..."
+      synchronize 
+        aggregateId 
+        (fun _ -> player3Events)
+        ([(fun _ -> player2Events); (fun _ -> player1Events)])
+        (fun events -> playerThreeEvents <- events)
+        fitnessFunction
+      |> ignore
+
+      printfn "End of synchronization."
+
+    while true do
+      async {
+        do! Async.Sleep synchronizeTickMilliseconds
+        synchronizeAll 
+          aggregateId 
+          (function 
+          | SomePublicEvent event -> true
+          | _ -> false)
+        |> ignore
+      }
+      |> Async.RunSynchronously
+      |> ignore
+  }
     
 [<EntryPoint>]
 let main argv = 
+
   [1; 2; 3]
   |> List.map playerLoop
+  |> List.append [synchronizeLoop] 
   |> Async.Parallel     
   |> Async.RunSynchronously 
   |> ignore
